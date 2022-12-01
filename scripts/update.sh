4055bce19ec02b613f104cea9ed196e2abab6ce5
Nicolas Béguier <nicolas.beguier@adevinta.com>
2022-12-01 08:17:21 +0100

    Update bootstrap and maj software-security-testing

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # https://getbootstrap.com/docs/5.0/getting-started/introduction/
-BOOTSTRAP_CSS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-BOOTSTRAP_JS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+BOOTSTRAP_CSS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+BOOTSTRAP_JS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
 # https://ionic.io/ionicons/usage
 IONICONS_JS="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
 
-OLD_BOOTSTRAP_CSS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-OLD_BOOTSTRAP_JS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+OLD_BOOTSTRAP_CSS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+OLD_BOOTSTRAP_JS="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
 OLD_IONICONS_JS="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
 
 find . -name '*.html' -exec sed -i.bak "s#${OLD_BOOTSTRAP_CSS}#${BOOTSTRAP_CSS}#g" {} \;
